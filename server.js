77789a1126cca2626f54aaf0ef8eb71641469b80
bhushan04 <30999828+bhushan04@users.noreply.github.com>
2017-08-20 13:20:48 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,8 +9,8 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/', function (req, res) {
-  res.sendFile(hello);
+app.get('/artical-one', function (req, res) {
+ res.sendFile(path.join(__dirname, 'ui', 'artical-one.html'));
 });
 
 app.get('/ui/style.css', function (req, res) {
